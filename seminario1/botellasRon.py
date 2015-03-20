39d88e6ac67fc784058879a19b8f1c506dc25301
DavidBlancoSD <david.blancocanizares@alum.uca.es>
2015-03-21 00:39:01 +0100

    Primer commit

---
+++ seminario1/botellasRon.py
@@ -0,0 +1,7 @@
+#99 botellas de ron
+
+botellas = 99
+while botellas>0:
+    print botellas, "bottles of beer on the wall, ", botellas, "bottles of beer. Take one down, pass it round, ", botellas - 1, "bottles of beer on the wall."
+    botellas = botellas - 1
+
